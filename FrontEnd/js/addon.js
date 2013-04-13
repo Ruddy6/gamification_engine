3f63f704564b5d907fdae4cf48e90436557cd006
Zannini <zannininicolas@gmail.com>
2013-04-13 10:57:53 +0200

    Ajout de la partie frontent

---
+++ FrontEnd/js/addon.js
@@ -0,0 +1,8 @@
+/*Fonctions génériques utiles pour tout le projet*/
+
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
+
