6608a0276c8d6833561d773a0e4ef11a14bd939c
ruddy6 <valentin.rudolf@gmail.com>
2013-05-05 22:03:44 +0200

    Mise à jour du server

--- gamificationEngine/server.js
+++ gamificationEngine/server.js
@@ -29,30 +29,34 @@ app.configure(function() {
 app.get('/applications', application.getAllApplications);
 app.post('/applications', application.addApplication);
 app.get('/applications/:id', application.getApplicationById);
+app.get('/applications/:id/players', application.getPlayers);
+app.get('/applications/:id/badges', application.getBadges);
 app.put('/applications/:id', application.updateApplication);
 app.delete('/applications/:id', application.deleteApplication);
 
 // BADGES
-app.get('/applications/:app_id/badges', badge.getAllBadgesApplication);
-app.get('/badges/:badge_id', badge.getBadgeById);
+app.get('/applications/:app_id/badges/:badge_id', badge.getBadgeById);
 app.get('/applications/:app_id/badges/levels/:level', badge.getBagdesByLevel);
+app.get('/applications/:app_id/badges/:badge_id/players', badge.getPlayers);
+app.get('/applications/:app_id/badges/:badge_id/application', badge.getApplication);
 app.post('/applications/:app_id/badge', badge.addBadge);
-app.post('/applications/:app_id/badges/:badge_id/player/:player_id', badge.addBadgeToPlayer);
-app.get('/applications/:app_id/badges/player/:player_id', badge.getBadgesOfPlayer);
 app.put('/applications/:app_id/badges/:badge_id', badge.updateBadge);
 app.delete('/applications/:app_id/badges/:badge_id', badge.deleteBadge);
 
 // EVENTS
-app.get('/applications/:app_id/events', event.getAllEventsApplication);
 app.get('/applications/:app_id/events/:event_id', event.getEventById);
 app.post('/applications/:app_id/players/:player_id/events', event.addEvent);
+app.get('/applications/:app_id/events/:event_id/player', event.getPlayer);
+app.get('/applications/:app_id/events/:event_id/application', event.getApplication);
 app.put('/applications/:app_id/events/:event_id', event.updateEvent);
 app.delete('/applications/:app_id/events/:event_id', event.deleteEvent);
 
 // PLAYERS
 app.post('/applications/:app_id/player', player.addPlayer);
-app.get('/applications/:app_id/players', player.getAllPlayersApplication);
 app.get('/applications/:app_id/players/:player_id', player.getPlayerById);
+app.post('/applications/:app_id/badge/:badge_id/player/:player_id', player.addBadge);
+app.get('/applications/:app_id/players/:player_id/badges', player.getBadges);
+app.get('/applications/:app_id/players/:player_id/events', player.getEvents);
 app.put('/applications/:app_id/players/:player_id', player.updatePlayer);
 app.delete('/applications/:app_id/players/:player_id', player.deletePlayer);
 
