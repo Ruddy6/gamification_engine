6e220f8c11818ccd2a09708f3867395601773ef3
ruddy6 <valentin.rudolf@gmail.com>
2013-05-10 16:49:15 +0200

    Mise à jour server

--- gamificationEngine/server.js
+++ gamificationEngine/server.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var mongo = require('mongodb');
 
 // On se connecte à la base de données
-// N'oubliez pas de lancer ~/mongodb/bin/mongod dans un terminal !
 mongoose.connect('mongodb://localhost/gamificationdb', function(err) {
     console.log("connect db mongoose");
     if (err) {
@@ -15,6 +14,7 @@ var express = require('express'),
         application = require('./routes/applications');
         badge = require('./routes/badges');
         event = require('./routes/events');
+        typeEvent = require('./routes/typeEvents');
         player = require('./routes/players');
         rule = require('./routes/rules');
 
@@ -24,6 +24,12 @@ app.configure(function() {
     app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
 });
+
+////////////////////////////////////////////////////////////////
+////                                                        //// 
+////    Revoir toutes les "signatures" des Web services     ////
+////                                                        ////
+////////////////////////////////////////////////////////////////
 
 // APPLICATIONS
 app.get('/applications', application.getAllApplications);
@@ -36,16 +42,20 @@ app.delete('/applications/:id', application.deleteApplication);
 
 // BADGES
 app.get('/applications/:app_id/badges/:badge_id', badge.getBadgeById);
-app.get('/applications/:app_id/badges/levels/:level', badge.getBagdesByLevel);
 app.get('/applications/:app_id/badges/:badge_id/players', badge.getPlayers);
 app.get('/applications/:app_id/badges/:badge_id/application', badge.getApplication);
 app.post('/applications/:app_id/badge', badge.addBadge);
 app.put('/applications/:app_id/badges/:badge_id', badge.updateBadge);
 app.delete('/applications/:app_id/badges/:badge_id', badge.deleteBadge);
 
+// TYPE EVENT
+app.get('/applications/:app_id/typeEvents/:typeEvent_id', typeEvent.getTypeEventById);
+app.post('/applications/:app_id/typeEvents', typeEvent.addTypeEvent);
+app.get('/applications/:app_id/typeEvents/:typeEvent_id/application', typeEvent.getApplication);
+
 // EVENTS
 app.get('/applications/:app_id/events/:event_id', event.getEventById);
-app.post('/applications/:app_id/players/:player_id/events', event.addEvent);
+app.post('/applications/:app_id/players/:player_id/typeEvents/:typeEvent_id/event', event.addEvent);
 app.get('/applications/:app_id/events/:event_id/player', event.getPlayer);
 app.get('/applications/:app_id/events/:event_id/application', event.getApplication);
 app.put('/applications/:app_id/events/:event_id', event.updateEvent);
@@ -54,14 +64,14 @@ app.delete('/applications/:app_id/events/:event_id', event.deleteEvent);
 // PLAYERS
 app.post('/applications/:app_id/player', player.addPlayer);
 app.get('/applications/:app_id/players/:player_id', player.getPlayerById);
-app.post('/applications/:app_id/badge/:badge_id/player/:player_id', player.addBadge);
+//app.post('/applications/:app_id/badge/:badge_id/player/:player_id', player.addBadge);
 app.get('/applications/:app_id/players/:player_id/badges', player.getBadges);
 app.get('/applications/:app_id/players/:player_id/events', player.getEvents);
 app.put('/applications/:app_id/players/:player_id', player.updatePlayer);
 app.delete('/applications/:app_id/players/:player_id', player.deletePlayer);
 
 // RULES
-app.post('/applications/:app_id/players/:player_id/badges/:badge_id/rule', rule.addRule);
+app.post('/applications/:app_id/badges/:badge_id/typeEvent/:typeEvent/rule', rule.addRule); // badge_id pour les tests --> normalement pas passé dans l'url
 app.get('/applications/:app_id/rules', rule.getAllRulesApplication);
 app.get('/applications/:app_id/rules/:rule_id', rule.getRuleById);
 app.put('/applications/:app_id/rules/:rule_id', rule.updateRule);
